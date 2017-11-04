2b259645bc852373ed5310b4c766256fdd694238
neethupl1985 <33363896+neethupl1985@users.noreply.github.com>
2017-11-03 22:36:47 -0400

    Add files via upload

---
+++ OCLCBeans.java
@@ -0,0 +1,317 @@
+package OCLCMINING;
+
+/**
+ * Created by lalithan on 11/2/17.
+ */
+
+public class OCLCBeans {
+
+    private String id;
+    private String created_date;
+    private String updated_date;
+    private String owner_institution;
+    private String source_institution;
+    private String collection_uid;
+    private String collection_name;
+    private String provider_uid;
+    private String provider_name;
+    private String oclcnum;
+    private String oclcnums;
+    private String issn;
+    private String eissn;
+    private String isbn;
+    private String workid;
+    private String title;
+    private String scrubtitle;
+    private String publisher;
+    private String url;
+    private String author;
+    private String content;
+    private String jkey;
+    private String bkey;
+    private String jsid;
+    private String pubtype;
+    private String coverage_start;
+    private String coverage_end;
+    private String openaccess;
+    private String open;
+    private String holdings_regid;
+    private String holdings_instid;
+    private String isbns;
+    private String user_oclcnum;
+    private String user_oclcnums;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(String created_date) {
+        this.created_date = created_date;
+    }
+
+    public String getUpdated_date() {
+        return updated_date;
+    }
+
+    public void setUpdated_date(String updated_date) {
+        this.updated_date = updated_date;
+    }
+
+    public String getOwner_institution() {
+        return owner_institution;
+    }
+
+    public void setOwner_institution(String owner_institution) {
+        this.owner_institution = owner_institution;
+    }
+
+    public String getSource_institution() {
+        return source_institution;
+    }
+
+    public void setSource_institution(String source_institution) {
+        this.source_institution = source_institution;
+    }
+
+    public String getCollection_uid() {
+        return collection_uid;
+    }
+
+    public void setCollection_uid(String collection_uid) {
+        this.collection_uid = collection_uid;
+    }
+
+    public String getCollection_name() {
+        return collection_name;
+    }
+
+    public void setCollection_name(String collection_name) {
+        this.collection_name = collection_name;
+    }
+
+    public String getProvider_uid() {
+        return provider_uid;
+    }
+
+    public void setProvider_uid(String provider_uid) {
+        this.provider_uid = provider_uid;
+    }
+
+    public String getProvider_name() {
+        return provider_name;
+    }
+
+    public void setProvider_name(String provider_name) {
+        this.provider_name = provider_name;
+    }
+
+    public String getOclcnum() {
+        return oclcnum;
+    }
+
+    public void setOclcnum(String oclcnum) {
+        this.oclcnum = oclcnum;
+    }
+
+    public String getOclcnums() {
+        return oclcnums;
+    }
+
+    public void setOclcnums(String oclcnums) {
+        this.oclcnums = oclcnums;
+    }
+
+    public String getIssn() {
+        return issn;
+    }
+
+    public void setIssn(String issn) {
+        this.issn = issn;
+    }
+
+    public String getEissn() {
+        return eissn;
+    }
+
+    public void setEissn(String eissn) {
+        this.eissn = eissn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getWorkid() {
+        return workid;
+    }
+
+    public void setWorkid(String workid) {
+        this.workid = workid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getScrubtitle() {
+        return scrubtitle;
+    }
+
+    public void setScrubtitle(String scrubtitle) {
+        this.scrubtitle = scrubtitle;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getJkey() {
+        return jkey;
+    }
+
+    public void setJkey(String jkey) {
+        this.jkey = jkey;
+    }
+
+    public String getBkey() {
+        return bkey;
+    }
+
+    public void setBkey(String bkey) {
+        this.bkey = bkey;
+    }
+
+    public String getJsid() {
+        return jsid;
+    }
+
+    public void setJsid(String jsid) {
+        this.jsid = jsid;
+    }
+
+    public String getPubtype() {
+        return pubtype;
+    }
+
+    public void setPubtype(String pubtype) {
+        this.pubtype = pubtype;
+    }
+
+    public String getCoverage_start() {
+        return coverage_start;
+    }
+
+    public void setCoverage_start(String coverage_start) {
+        this.coverage_start = coverage_start;
+    }
+
+    public String getCoverage_end() {
+        return coverage_end;
+    }
+
+    public void setCoverage_end(String coverage_end) {
+        this.coverage_end = coverage_end;
+    }
+
+    public String getOpenaccess() {
+        return openaccess;
+    }
+
+    public void setOpenaccess(String openaccess) {
+        this.openaccess = openaccess;
+    }
+
+    public String getOpen() {
+        return open;
+    }
+
+    public void setOpen(String open) {
+        this.open = open;
+    }
+
+    public String getHoldings_regid() {
+        return holdings_regid;
+    }
+
+    public void setHoldings_regid(String holdings_regid) {
+        this.holdings_regid = holdings_regid;
+    }
+
+    public String getHoldings_instid() {
+        return holdings_instid;
+    }
+
+    public void setHoldings_instid(String holdings_instid) {
+        this.holdings_instid = holdings_instid;
+    }
+
+    public String getIsbns() {
+        return isbns;
+    }
+
+    public void setIsbns(String isbns) {
+        this.isbns = isbns;
+    }
+
+    public String getUser_oclcnum() {
+        return user_oclcnum;
+    }
+
+    public void setUser_oclcnum(String user_oclcnum) {
+        this.user_oclcnum = user_oclcnum;
+    }
+
+    public String getUser_oclcnums() {
+        return user_oclcnums;
+    }
+
+    public void setUser_oclcnums(String user_oclcnums) {
+        this.user_oclcnums = user_oclcnums;
+    }
+
+
+}
